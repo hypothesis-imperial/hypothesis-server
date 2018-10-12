b369b5b8fdd650d69e80933efb8748d94906d175
Romain <rd2016@ic.ac.uk>
2018-10-12 13:13:53 +0100

    implemented github webhook and get_commit route

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
+import os
+from git import Repo
+
 
 application = Flask(__name__)
 app = application
@@ -7,6 +11,28 @@ app = application
 @app.route('/')
 def hello():
     return "Hello World!"
+
+
+@app.route('/webhook', methods=['POST'])
+def on_git_push():
+    if os.path.exists("code"):
+        os.rmdir("code")
+    os.makedirs("code")
+    data = json.loads(request.data)
+    url = data["repository"]["html_url"]
+    Repo.clone_from(url, "code")
+
+    return 'OK'
+
+
+@app.route('/get_commit_hash', methods=['GET'])
+def get_commit_hash():
+    repo = Repo("code")
+    sha = repo.head.object.hexsha
+
+    return jsonify({
+            "sha": sha
+            })
 
 
 if __name__ == '__main__':
