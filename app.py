18d2a687cc64f5a6883e35657f64cd529cac32a6
Romain <rd2016@ic.ac.uk>
2018-10-16 18:06:22 +0100

    on receiving new code, now runs hypothesis tests once and stores result in a txt file

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import json
 import os
+import subprocess
+import shutil
 from git import Repo
 
 
@@ -16,12 +18,19 @@ def hello():
 @app.route('/webhook', methods=['POST'])
 def on_git_push():
     if os.path.exists("code"):
-        os.rmdir("code")
+        shutil.rmtree("code", ignore_errors=True)
+    if os.path.isfile("results.txt"):
+        os.remove("results.txt")
     os.makedirs("code")
     data = json.loads(request.data)
     url = data["repository"]["html_url"]
     Repo.clone_from(url, "code")
-
+    os.chdir("code")
+    output = subprocess.run(['pytest', '-m', 'hypothesis', "--hypothesis-show-statistics"], universal_newlines=True, stdout=subprocess.PIPE)
+    os.chdir("..")
+    f = open("results.txt", "w+")
+    f.write(output.stdout)
+    f.close()
     return 'OK'
 
 
@@ -29,7 +38,6 @@ def on_git_push():
 def get_commit_hash():
     repo = Repo("code")
     sha = repo.head.object.hexsha
-
     return jsonify({
             "sha": sha
             })
