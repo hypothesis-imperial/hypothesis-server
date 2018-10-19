cea127faec1cf4259640428b86824f430fb058b9
Jack Pordi <jackel119@gmail.com>
2018-10-19 15:43:22 +0100

    async fuzzing

--- hypothesisfuzzer/app.py
+++ hypothesisfuzzer/app.py
@@ -3,43 +3,37 @@ import json
 import os
 import subprocess
 import shutil
+import threading
 from git import Repo
 
 app = Flask(__name__)
+
+current_fuzzing_task = None
 
 
 @app.route('/webhook', methods=['POST'])
 def on_git_push():
     if os.path.exists("code"):
         shutil.rmtree("code", ignore_errors=True)
+
     if os.path.isfile("results.txt"):
         os.remove("results.txt")
     os.makedirs("code")
     data = json.loads(request.data)
     url = data["repository"]["html_url"]
     Repo.clone_from(url, "code")
-    os.chdir("code")
-    for i in range(10):
-        output = subprocess.run(['pytest', '-m', 'hypothesis',
-                                "--hypothesis-show-statistics"],
-                                universal_newlines=True,
-                                stdout=subprocess.PIPE)
-        write_to_results(output)
+
+    current_fuzzing_task = AsyncFuzzingTask()
+    current_fuzzing_task.start()
+
     return 'OK'
-
-
-def write_to_results(output):
-    os.chdir("..")
-    f = open("results.txt", "a")
-    f.write(output.stdout)
-    f.close()
-    os.chdir("code")
 
 
 @app.route('/get_commit_hash', methods=['GET'])
 def get_commit_hash():
     repo = Repo("code")
     sha = repo.head.object.hexsha
+
     return jsonify({
             "sha": sha
             })
@@ -52,3 +46,24 @@ class Fuzzer:
 
     def run(self, **kwargs):
         self.app.run(**kwargs)
+
+
+class AsyncFuzzingTask(threading.Thread):
+
+    def __init__(self):
+        super(AsyncFuzzingTask, self).__init__()
+        pass
+
+    def run(self):
+        def write_to_results(output):
+            f = open("results.txt", "a")
+            f.write(output.stdout)
+            f.close()
+
+        for i in range(10):
+            output = subprocess.run(['pytest', '-m', 'hypothesis',
+                                    "--hypothesis-show-statistics"],
+                                    universal_newlines=True,
+                                    stdout=subprocess.PIPE)
+            write_to_results(output)
+            print('Did one iteration!')
