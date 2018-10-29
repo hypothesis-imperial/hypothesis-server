5982db31fca536e3def0d4a865aa5f8b45f33a1a
lanyielaine <elaineyilan@foxmail.com>
2018-10-29 16:56:49 +0000

    showing a dummy json

--- front-end/hypothesis-app/src/App.js
+++ front-end/hypothesis-app/src/App.js
@@ -6,23 +6,25 @@ class App extends Component {
 
   render() {
     var __template = require('./templates/dashboard.template');
-    // var template = { __html: __html };
+
+
+    var data = require('./dummy/dummy.json');
+    var test = JSON.stringify(data);
+
+    console.log(data);
 
     const render = (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>
-            Hello
-          </p>
-        </header>
-      </div>
+       <div className="App">
+         <header className="App-header">
+           <img src={logo} className="App-logo" alt="logo" />
+           <p>
+             {test}
+           </p>
+         </header>
+       </div>
     )
 
-    return __template;
+    return render;
   }
 }
 
