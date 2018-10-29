095a3f078e6896d09ee4100d19485b241edbcb54
lanyielaine <elaineyilan@foxmail.com>
2018-10-29 18:54:56 +0000

    check for longer json

--- front-end/hypothesis-app/src/App.js
+++ front-end/hypothesis-app/src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './dummy/dummy.json';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+       data2: data
+    }
+  }
+
   render() {
-    var __template = require('./templates/dashboard.template');
 
+    // var __template = require('./templates/dashboard.template');
 
-    var data = require('./dummy/dummy.json');
-    var test = JSON.stringify(data);
+    //var data = require('./dummy/dummy.json');
+    // var item = data.map((case) =>
+    //   <li>{case}</li>
+    // );
+
+    var test = JSON.stringify(this.state.data2);
 
     console.log(data);
 
     const render = (
+
        <div className="App">
          <header className="App-header">
            <img src={logo} className="App-logo" alt="logo" />
            <p>
              {test}
            </p>
+           <ul>
+            <li>test1</li>
+           </ul>
          </header>
        </div>
     )
