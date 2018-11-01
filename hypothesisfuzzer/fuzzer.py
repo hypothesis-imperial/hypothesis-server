0d6d8ed8109f85202def976501aed2b55b722f00
Jack Pordi <jackel119@gmail.com>
2018-11-01 19:06:55 +0000

    Fix chdir bug again x4

--- hypothesisfuzzer/fuzzer.py
+++ hypothesisfuzzer/fuzzer.py
@@ -58,10 +58,8 @@ class Fuzzer:
                                    universal_newlines=True,
                                    stdout=subprocess.PIPE)
                     print('Did one iteration!')
-                os.chdir("..")
                 print('Stopped now')
 
-            os.chdir("..")
             self.current_fuzzing_task = threading.Thread(target=fuzz, args=())
             self.current_fuzzing_task.start()
             os.chdir("..")
