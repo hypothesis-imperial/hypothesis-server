92868cc66b8ad35a3a246cc9f11ec34d549a9026
dka316 <dka316@ic.ac.uk>
2018-11-13 23:32:55 +0000

    Added improved messages and split logs into more levels

--- hypothesisfuzzer/fuzz_server.py
+++ hypothesisfuzzer/fuzz_server.py
@@ -1,28 +1,26 @@
 import json
+import logging
 import os
 import yaml
-import logging
 
 from .fuzzing import RepoFuzzer
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask import Flask, request, send_from_directory
-from .errors import (
-    ConfigMissingOptionException,
-)
-
-
-logging.basicConfig(datefmt='%d-%b-%y %H:%M:%S',
-                    filename='fuzz_server.log',
-                    format='%(asctime)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
+from .errors import ConfigMissingOptionException
 
 
 class FuzzServer:
 
     def __init__(self, config_path='config.yml'):
 
-        logging.info('Fuzz server initialising.')
+        logging.basicConfig(datefmt='%d-%b-%y %H:%M:%S'
+                           , filename='fuzz_server.log'
+                           , format='%(asctime)s - %(levelname)s - %(message)s'
+                           , level=logging.DEBUG)
+
+        self.log = logging.getLogger('logger')
+        self.log.debug('Initialising fuzzing server.')
 
         self.app = Flask(__name__, static_url_path='/build')
         CORS(self.app)
@@ -37,69 +35,70 @@ class FuzzServer:
         for name, repo_config in self.config['repos'].items():
             self.fuzzer = RepoFuzzer(name, repo_config)
 
-        logging.info('Fuzz server initialised.')
+        self.log.debug('Fuzzing server initialised.')
 
     def run(self, **kwargs):
 
-        logging.info('Fuzz server started running.')
+        self.log.debug('Fuzzing server started running.')
 
         self.db.create_all()
         self._setup_routes()
 
         self.app.run(**kwargs)
 
-        logging.info('Fuzz server stopped running.')
+        self.log.debug('Fuzzing server stopped running.')
 
     def _setup_routes(self):
 
-        logging.info('Fuzz server started setting up routes')
+        self.log.debug('Setting up routes.')
 
         @self.app.route('/webhook', methods=['POST'])
         def on_git_push():
-            logging.info('Fuzz server _setup_routes(): on_git_push()')
+            self.log.info('Git push occurred.')
             return self.fuzzer.on_webhook(json.loads(request.data))
 
         @self.app.route('/get_commit_hash', methods=['GET'])
         def get_commit_hash():
-            logging.info('Fuzz server _setup_routes(): get_commit_hash()')
+            self.log.info('Getting commit hash.')
             return self.fuzzer.get_commit_hash()
 
         @self.app.route('/', methods=['GET'])
         def home():
-            logging.info('Fuzz server _setup_routes(): home()')
+            self.log.info('Home.')
             return send_from_directory('build', 'index.html')
 
         @self.app.route('/<path:path>', methods=['GET'])
         def serve_static(path):
-            logging.info('Fuzz server _setup_routes(): serve_static()')
+            self.log.info('Serve static.')
             return send_from_directory('build', path)
 
         @self.app.route('/get_errors', methods=['GET'])
         def get_errors():
-            logging.info('Fuzz server _setup_routes(): get_errors()')
+            self.log.info('Getting errors.')
             return self.fuzzer.get_errors()
 
-        logging.info('Fuzz server stopped setting up routes')
+        self.log.debug('Routes set up.')
 
     def _load_config(self, config_path):
 
-        logging.info('Fuzz server started loading configurations')
+        self.log.debug('Loading configurations.')
 
         try:
-            logging.info('Fuzz server _load_config(): file open')
             with open(config_path) as file:
+                self.log.debug('Opening file config.yml.')
                 self.config = yaml.load(file)
 
                 if 'repos' not in self.config:
-                    logging.error('Fuzz server _load_config(): ' +
-                                  'Configuration file missing' +
-                                  'a repos attribute.', exc_info=True)
+                    self.log.error('Configuration file missing repos attribute.'
+                                  , exc_info=True)
                     raise \
                         ConfigMissingOptionException("Configuration file" +
                                                      "missing a 'repos'" +
                                                      "attribute")
+                self.log.debug('File config.yml loaded.')
         except FileNotFoundError:
-            logging.error('Fuzz server _load_config(): ' +
-                          'config.yml file not found.', exc_info=True)
+            self.log.error('File config.yml not found.', exc_info=True)
             raise FileNotFoundError('config.yml file not found. ' +
                                     'Create one or specify config path.')
+
+        self.log.debug('Configurations loaded.')
--- hypothesisfuzzer/fuzzing/repo_fuzzer.py
+++ hypothesisfuzzer/fuzzing/repo_fuzzer.py
@@ -1,9 +1,10 @@
-import os
 import json
+import logging
+import os
 import shutil
-import virtualenv
 import subprocess
 import threading
+import virtualenv
 
 from flask import jsonify
 from git import Repo as GitRepo
@@ -16,73 +17,125 @@ from ..errors import (
 class RepoFuzzer:
 
     def __init__(self, name, config):
+
+        self.log = logging.getLogger('logger')
+        self.log.debug('Initialising repository fuzzer.')
+
         self.name = name
         self.config = config
         self._clone_git(config['git_url'])
         self._create_venv()
         self._start_fuzzing()
 
+        self.log.debug('Repository fuzzer initialised.')
+
     def on_webhook(self, payload):
+
+        self.log.debug('Webhook event began.')
+
         # Check if repo is the same name as the one set in config
+        payload_name = payload["repository"]["name"]
+        config_name = self.config['repo_name']
         try:
-            if payload["repository"]["name"] != self.config['repo_name']:
+            if payload_name != config_name:
+                self.log.warning('Repository %s is different from %s.',
+                                 payload_name,
+                                 config_name)
                 return 'OK'
         except KeyError:
+            self.log.info('Repository %s is as configured.', payload_name)
             pass
 
         try:
+            self.log.debug('Cloning repository %s.', payload_name)
             self._clone_git(self.config['git_url'])
+            self.log.debug('Repository %s cloned.', payload_name)
         except Exception:
-            return generic_error(msg="Error cloning Git Repo! " +
+            self.log.error('Unable to access repository %s for cloning.',
+                           payload_name)
+            return generic_error(msg="Error cloning Git repository! " +
                                      "Please ensure you have access.")
 
         self._stop_fuzzing()
+        self.log.debug('Old repository fuzzing stopped.')
         self._create_venv()
+        self.log.debug('Virtual environment created and fuzzing restarted.')
         self._start_fuzzing()
+
+        self.log.debug('Webhook event ended.')
 
         return 'OK'
 
     def get_commit_hash(self):
 
+        self.log.debug('Getting commit hash for repository %s.', self.name)
+
         if not os.path.exists(self.name):
+            self.log.error('When getting commit hash, path of %s not found.',
+                           self.name)
             return no_code_dir_error()
         repo = GitRepo(self.name)
         sha = repo.head.object.hexsha
+
+        self.log.debug('Commit hash for repository %s obtained.', self.name)
 
         return jsonify({
             "sha": sha
         })
 
     def get_errors(self):
+
+        self.log.debug('Getting errors for repository %s.', self.name)
+
         if not os.path.exists(self.name):
+            self.log.error('When getting errors, path of %s not found.',
+                           self.name)
             return no_code_dir_error()
         with open(self.name+'/data.txt', 'r') as file_data:
+            self.log.debug('Errors for repository %s obtained.', self.name)
             return jsonify(json.load(file_data))
 
     def _clone_git(self, git_url):
-        # Delete old code folder
 
+        self.log.debug('Deleting repository %s.', self.name)
+
+        # Delete old code folder
         if os.path.exists(self.name):
             shutil.rmtree(self.name, ignore_errors=True)
 
         os.makedirs(self.name)
         GitRepo.clone_from(git_url, self.name)
 
+        self.log.debug('Repository %s cloned.', self.name)
+
     def _create_venv(self):
+
+        self.log.debug('Creating virtual environment for repository %s.',
+                      self.name)
 
         os.chdir(self.name)
         assert os.path.basename(os.getcwd()) == self.name
         virtualenv.create_environment('venv')
         subprocess.run(['venv/bin/pip',
                         'install', '-r', 'requirements.txt'])
-        os.chdir('..')
+        os.chdiry("..")
+
+        self.log.debug('Virtual environment for repository %s created.',
+                      self.name)
 
     def _stop_fuzzing(self):
+
+        self.log.debug('Stopping fuzzing for repository %s.', self.name)
+
         if self._current_fuzzing_task:
             self._current_fuzzing_task.running = False
             self._current_fuzzing_task.join()
 
+        self.log.debug('Fuzzing for repository %s stopped.', self.name)
+
     def _start_fuzzing(self):
+
+        self.log.debug('Starting fuzzing for repository %s.', self.name)
 
         os.chdir(self.name)
         assert os.path.basename(os.getcwd()) == self.name
@@ -92,16 +145,23 @@ class RepoFuzzer:
         self._current_fuzzing_task.start()
         os.chdir("..")
 
+        self.log.debug('Fuzzing for repository %s started.', self.name)
+
     def _fuzz_task(self):
+
+        self.log.debug('Fuzzing task of repository %s.', self.name)
 
         assert os.path.basename(os.getcwd()) == self.name
 
         iteration = 0
 
         while getattr(self._current_fuzzing_task, "running", True):
+            self.log.info('Fuzzing iteration %s.', iteration)
             subprocess.run(['venv/bin/pytest'],
                            universal_newlines=True,
                            stdout=subprocess.PIPE)
             print('Fuzzing iteration: ', iteration)
             iteration += 1
         print('Fuzzing stopped after', iteration, 'iterations')
+
+        self.log.debug('Task of repository %s fuzzed.', self.name)
