66b3472216f18af033ed36866faac6e4ba250eb0
Jack Pordi <jackel119@gmail.com>
2018-11-16 14:35:21 +0000

    Develop (#62)
    * 031/separate server fuzz logic refactor (#54)
    
    * Move setup routes to another method
    
    * Move business logic out
    
    * Attempt #1 at separating server and fuzzing logic
    
    * Fixed dir bug on start
    
    * debug before comitting refactor
    
    * Fix flake8
    
    * Review fixes
    
    * 030/documentation (#55)
    
    * Move setup routes to another method
    
    * Move business logic out
    
    * Attempt #1 at separating server and fuzzing logic
    
    * Fixed dir bug on start
    
    * debug before comitting refactor
    
    * Rewrite readme
    
    * Add basic description and howto
    
    * Basic 'about' README
    
    * added example yml file
    
    * Minor changes
    
    * Remove redundant stuff, add Authors, License, and Acknowledgements
    
    * Fix flake8
    
    * Review fixes
    
    * Update README.md
    
    * update author in readme.md
    
    * update README.md
    
    * Update README.md
    
    Removed line break
    
    * Code dir name from repo name + enable virtualenv pytest (#57)
    
    * Code dir name from repo name + enable virtualenv pytest
    
    * Bug fixes
    
    * wip
    
    * changed readme, updates sample comfig
    
    * captured a sample payload for testing
    
    * Reformat sample payload
    
    * wip
    
    * added datetime of fuzz start
    
    * wip
    
    * changed directory logic
    
    * Start fuzzing on run instead of init
    
    * Remove debug in flask
    
    * added all info route
    
    * added all info route
    
    * used return jsonify and changed variable name
    
    * Attempt to fix indentations
    
    * 035/logging (#60)
    
    * Added basic logger
    
    * Removed unnecessary line
    
    * Added improved messages and split logs into more levels
    
    * Attempt to match flake8 format
    
    * Reduced logging levels
    
    * Removed redundant logs
    
    * Added SysLogHandler for Linux
    
    * Modified levels and globalised loggers
    
    * Fixed indentations
    
    * Fixed indentations
    
    * Level changes

--- hypothesisfuzzer/fuzz_server.py
+++ hypothesisfuzzer/fuzz_server.py
@@ -1,19 +1,35 @@
 import json
+import logging
+import logging.handlers
 import os
 import yaml
 
+from .errors import ConfigMissingOptionException
 from .fuzzing import RepoFuzzer
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask import Flask, request, send_from_directory
-from .errors import (
-    ConfigMissingOptionException,
-)
+from flask import Flask, request, send_from_directory, jsonify
+from sys import platform
+
+
+logging.basicConfig(datefmt='%d-%b-%y %H:%M:%S',
+                    filename='fuzz_server.log',
+
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
+
+if platform.startswith('linux'):
+    handler = logging.handlers.SysLogHandler(address='/dev/log')
+    logging.getLogger('logger').addHandler(handler)
+
+logger = logging.getLogger(__name__)
 
 
 class FuzzServer:
 
     def __init__(self, config_path='config.yml'):
+
+        logger.debug('Initialising fuzzing server.')
 
         self.app = Flask(__name__, static_url_path='/build')
         CORS(self.app)
@@ -22,28 +38,61 @@ class FuzzServer:
         self.app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
         self._load_config(config_path)
+        self._init_fuzzers()
         self.db = SQLAlchemy(self.app)
-        self.current_fuzzing_task = None
 
-        for name, repo_config in self.config['repos'].items():
-            self.fuzzer = RepoFuzzer(name, repo_config)
+        logger.info('Fuzzing server initialised.')
 
     def run(self, **kwargs):
+
+        logger.info('Fuzzing server started running.')
 
         self.db.create_all()
         self._setup_routes()
 
+        for (name, owner), fuzzer in self.fuzzers.items():
+            fuzzer.start()
+
         self.app.run(**kwargs)
+
+        logger.debug('Fuzzing server stopped running.')
 
     def _setup_routes(self):
 
+        logger.info('Setting up routes.')
+
+        @self.app.route('/all_info', methods=['GET'])
+        def get_data():
+            repo_infos = {}
+
+            for (name, owner), fuzzer in self.fuzzers.items():
+                with open(name + '.json') as f:
+                    data = json.load(f)
+                    repo_infos[name] = data
+
+            return jsonify(repo_infos)
+
         @self.app.route('/webhook', methods=['POST'])
         def on_git_push():
-            return self.fuzzer.on_webhook(json.loads(request.data))
+            logger.debug('Git push for repository %s occurred.',
+                         self.fuzzer.name)
+            data = json.loads(request.data)
+            name = data['repository']['name']
+            owner = data['repository']['owner']['name']
 
-        @self.app.route('/get_commit_hash', methods=['GET'])
-        def get_commit_hash():
-            return self.fuzzer.get_commit_hash()
+            try:
+                fuzzer = self.fuzzers[(name, owner)]
+
+                return fuzzer.on_webhook(data)
+            except KeyError:
+                logger.error('Server not configured to fuzz this repository.',
+                             exc_info=True)
+                err_message = (
+                    'Hypothesis server has not been configured to'
+                    'fuzz this repository.'
+                )
+
+                return err_message, 404
 
         @self.app.route('/', methods=['GET'])
         def home():
@@ -53,20 +102,64 @@ class FuzzServer:
         def serve_static(path):
             return send_from_directory('build', path)
 
-        @self.app.route('/get_errors', methods=['GET'])
+        @self.app.route('/get_errors', methods=['POST'])
         def get_errors():
-            return self.fuzzer.get_errors()
+            data = json.loads(request.data)
+            name = data['name']
+            owner = data['owner']
+            try:
+                fuzzer = self.fuzzers[(name, owner)]
+
+                return jsonify(fuzzer.get_errors())
+            except KeyError:
+                logger.error('Server not configured to fuzz this repository.')
+                err_message = (
+                    'Hypothesis server has not been configured to'
+                    'fuzz this repository.'
+                )
+
+                return err_message, 404
+
+        logger.debug('Routes set up.')
 
     def _load_config(self, config_path):
+
+        logger.debug('Loading configurations.')
+
         try:
             with open(config_path) as file:
+                logger.info('Opening file config_path.')
                 self.config = yaml.load(file)
 
                 if 'repos' not in self.config:
-                    raise \
-                        ConfigMissingOptionException("Configuration file" +
-                                                     "missing a 'repos'" +
-                                                     "attribute")
+                    logger.error('Configuration file missing repos.',
+                                 exc_info=True)
+                    raise ConfigMissingOptionException("Configuration file" +
+                                                       "missing a 'repos'" +
+                                                       "attribute")
+                logger.info('File config_path loaded.', exc_info=True)
         except FileNotFoundError:
+            logger.error('File config.yml not found.', exc_info=True)
             raise FileNotFoundError('config.yml file not found. ' +
                                     'Create one or specify config path.')
+
+        logger.info('Server Configuration loaded.')
+
+    def _init_fuzzers(self):
+
+        logger.debug('Initialising fuzzers.')
+
+        self.fuzzers = {}
+
+        for repo, repo_config in self.config['repos'].items():
+
+            repo_name = repo_config['name']
+            repo_owner = repo_config['owner']
+
+            self.fuzzers[(repo_name, repo_owner)] = RepoFuzzer(repo_name,
+                                                               repo_config)
+
+        logger.info('Fuzzers initialised.')
+
+
+logging.shutdown()
